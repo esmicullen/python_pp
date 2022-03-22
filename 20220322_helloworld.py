1d57fbccec3a7ae27b1bacece708ba9ea0b3ab41
Cullen Esmi <jegayun@gmail.com>
2022-03-22 10:13:57 +0900

    변수선언과 초기화, 여러 가지 출력법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,5 +1,9 @@
 from builtins import print
 
 print("Hello,World")
-print('Hello,WOOZI')
+name='우지'
+print('Hello,'+name+'!') #자바형식
+print('Hello, %s!'% name) #씨형식
+print('Hello, {}!' .format(name)) #format함수
+print(f'Hello, {name}!') #f-string형식
 print("WOOZI's Mixtape name is Ruby.")
