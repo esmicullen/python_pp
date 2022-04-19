dbd846e4b068ca42ac5cb99afd8877ea8c915ad2
Cullen Esmi <jegayun@gmail.com>
2022-04-19 10:09:52 +0900

    str 연습, 문자열 함수(len, count, upper, lower, strip, replace, find, index), in, split, join

--- 20220419_str.py
+++ 20220419_str.py
@@ -34,3 +34,34 @@ print(f'{stu_num[0:]}번') #start:end-1 [start:]: 끝까지
 print(f'{stu_num[:-2]}번') #start:end-1 [:end-1]: 앞까지
 print(f'{stu_num[:]}번') #start:end-1 [:]: 앞~끝까지
 
+#문자열 함수
+print(f'길이: {len(stu_num)}') #4
+print(f'2개수: {stu_num.count("2")}') #2
+print(f'{"seventeen darling".upper()}')
+print(f'{"seventeen darling".lower()}')
+s = "   NCT dream buffering   "
+print(f'{s.strip()}')
+print(f'{s.lstrip()}')
+print(f'{s.rstrip()}')
+print(f'{s.find("d")}')
+#print(f'{s.find("z")}') #없으면 에러
+print(f'{s.replace("buffering","hello future")}')
+print(s)
+
+print('e' in s)
+print('z' in s)
+
+#split, join
+ip = '10.253.123.119'
+ip_list=ip.split('.')
+print(ip_list)
+names = '양다연,최자윤,임채영,이예진,인소리'
+name_list = names.split(',')
+print(name_list)
+print(name_list[2])
+print(name_list[2:4])
+ip_list_str = '::'.join(ip_list)
+print(ip_list_str)
+name_list_str = '|'.join(name_list)
+print(name_list_str)
+
