1099a05640c106f0957378362021a9a0e4cddbef
Cullen Esmi <jegayun@gmail.com>
2022-06-21 10:19:57 +0900

    함수 연습

--- 20220614_function.py
+++ 20220614_function.py
@@ -25,13 +25,13 @@ def my_print(age):
 
 
 def my_print2(name, age):
-    print(name + ' : ' + age + '살 입니다')
+#    print(name + ' : ' + age + '살 입니다')
     print(name, ': ', age, '살 입니다')
     print(f'{name} : {age}살 입니다')
 
 
 def my_print3(name, age, group):
-    print(name + ' : ' + age + '살 입니다' + group + '소속입니다.')
+#    print(name + ' : ' + age + '살 입니다' + group + '소속입니다.')
     print(name, ': ', age, '살 입니다', group, ' 소속입니다.')
     print(f'{name} : {age}살 입니다 {group} 소속입니다')
 
@@ -44,12 +44,12 @@ print('-' * 20)
 
 
 def my_print4(name, age, group='세븐틴'):  # 기본값있는 파라미터
-    print(name + ' : ' + age + '살 입니다' + group + '소속입니다.')
+#    print(name + ' : ' + age + '살 입니다' + group + '소속입니다.')
     print(name, ': ', age, '살 입니다', group, ' 소속입니다.')
     print(f'{name} : {age}살 입니다 {group} 소속입니다')
 
 
-my_print4('이지훈', age=27, group='리더즈')
+#my_print4('이지훈', age=27, group='리더즈')
 print('-' * 20)
 
 
@@ -67,5 +67,58 @@ def my_print6(name, *args):
 my_print5('이지훈', 27, '세븐틴', 'Ruby')
 my_print6('이지훈', 27, '세븐틴', 'Ruby')
 print('-' * 20)
+def say(na, msg = '안녕하세요',feeling='❤'):
+    print(f'{na}, {msg} {feeling}')
+say('가현',feeling = '✌')
 print('-' * 20)
+def fn(a,b=[]):
+    b.append(a)
+    print(b)
+fn(3) #[3]
+fn(5) #[3,5]
+fn(10,[1]) #[1,10]
+fn(7) #[3,5,7]
+print('-' * 20)
+say('현진','Sorry','🙏')
+
+'''
+def plus20(age):
+    print(age+20)
+a = plus20(18)
+print(a) #None 출력, 리턴값이 없음!
+'''
+
+def plus20(age):
+    return age+20
+a = plus20(18)
+print(a)
+
+print('-' * 20)
+#전화번호 앞 자리 (지역번호)와 맨 뒤 네자리 출력하기
+
+def tel(num):
+    index = num.find('-')
+    f = num[:index]
+    b = num[-4:]
+    return f,b
+# front = '010'
+# back = '2390'
+front, back = tel('010-3977-2390')
+print(f'앞 : {front}, 뒤 : {back}')
+print('-' * 20)
+def min_max(리스트):
+    if len(리스트) < 0 :
+        return None
+    max_i = 리스트[0]
+    min_i = 리스트[0]
+    for b in 리스트:
+        if max_i<=b: max_i = b
+        elif min_i>=b: min_i = b
+    return min_i,max_i
+min_value, max_value = min_max([3,31,1,6,5,-6])
+
+print(f'{min_value} , {max_value}')
+
+print('-' * 20)
+
 print('-' * 20)
